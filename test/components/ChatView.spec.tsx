b488520e6a13f302c94c7640bbf285a569ce7aad
Cubas Wiemer, Uwe <Uwe.Cubas-Wiemer@tecalliance.net>
2017-08-07 13:05:41 +0200

    Added tests

---
+++ test/components/ChatView.spec.tsx
@@ -0,0 +1,47 @@
+
+import * as React from 'react'
+import { ChatView } from '../../src/components/ChatView'
+import * as enzyme from 'enzyme'
+import * as moment from 'moment'
+import toJson from 'enzyme-to-json'
+
+describe('ChatView', function () {
+   let component: enzyme.ReactWrapper<any, any>
+
+   beforeEach(function () {
+     component = enzyme.mount(
+      <ChatView 
+        messages={[]}
+        selfNick='nick'
+      />
+    )
+   })
+
+  it('should render', function () {
+    expect(toJson(component)).toMatchSnapshot()
+  })
+
+  it('should render messages', function () {
+    component.setProps({ messages: [{ name: 'name', text: 'text', date: moment.utc('2017-01-01T00:00:00.000Z') }] })
+    expect(toJson(component)).toMatchSnapshot()
+  })
+
+  it('should render message as self', function () {
+    component.setProps({ messages: [{ name: 'nick', text: 'text', date: moment.utc('2017-01-01T00:00:00.000Z') }] })
+    expect(toJson(component)).toMatchSnapshot()
+  })
+
+  it('should not scroll to bottom', function () {
+    let instance = component.instance() as any;
+    instance.refs.container = { scrollTop: 0, scrollHeight: 100, clientHeight: 0 };
+    component.setProps({ messages: [{ name: 'nick', text: 'text', date: moment.utc('2017-01-01T00:00:00.000Z') }] })
+    expect(toJson(component)).toMatchSnapshot()
+  })
+
+  it('should return when no container', function () {
+    let instance = component.instance() as any;
+    instance.refs.container = undefined;
+    instance.scrollAtBottom = false;
+    component.setProps({ messages: [{ name: 'nick', text: 'text', date: moment.utc('2017-01-01T00:00:00.000Z') }] })
+  })
+})

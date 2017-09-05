f9e83437166c0e59dc2f2a5459d91c0b35ad9ecd
Cubas Wiemer, Uwe <Uwe.Cubas-Wiemer@tecalliance.net>
2017-09-05 11:40:01 +0200

    Added middle click close tab

--- src/components/Tab.tsx
+++ src/components/Tab.tsx
@@ -39,16 +39,32 @@ const divStyleActive: React.CSSProperties = {
 };
 
 export class Tab extends React.Component<IProps, null> {
+  constructor() {
+    super();
+
+    this.handleClick = this.handleClick.bind(this);
+    this.handleMiddleClick = this.handleMiddleClick.bind(this);
+  }
+
   public render() {
     return (
-      <div style={this.props.isActive ? divStyleActive : divStyle}>
+      <div onMouseDown={this.handleMiddleClick} style={this.props.isActive ? divStyleActive : divStyle}>
         <div
-          onClick={this.props.tabClick.bind(this, this.props.tabName)}
+          onClick={this.handleClick}
           style={{ transform: 'skew(15deg)' }}
         >{this.props.tabName}
         </div>
         <div onClick={this.props.closeTab.bind(this, this.props.tabName)} style={xStyle}>&times;</div>
       </div>
     );
+  }
+  private handleMiddleClick(event) {
+    const type = event.nativeEvent.which;
+    if (type !== 2) { return; }
+    this.props.closeTab(this.props.tabName);
+  }
+
+  private handleClick(event) {
+    this.props.tabClick(this.props.tabName);
   }
 }
